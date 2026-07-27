f27a70127054caac6272092a05a9f1e084737eec
Angelo <zebhastyanangelo@gmail.com>
2026-07-27 17:52:11 -0400

    fix(map): cast via unknown para delete _getIconUrl en Leaflet

--- menugran/src/components/map/RiderTracker.tsx
+++ menugran/src/components/map/RiderTracker.tsx
@@ -6,7 +6,7 @@ import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
 // Fix Leaflet default icon issue in Next.js
-delete (L.Icon.Default.prototype as Record<string, unknown>)._getIconUrl;
+delete (L.Icon.Default.prototype as unknown as Record<string, unknown>)._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png",
   iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png",
